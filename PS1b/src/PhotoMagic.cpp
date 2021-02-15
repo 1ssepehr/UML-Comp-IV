ae9039b7964024ac6fba0eed301cd9ce12c8fd39
Sepehr Madani <ssepehrmadani@gmail.com>
2021-02-15 02:03:50 -0500

    Add progress messages

--- PS1b/src/PhotoMagic.cpp
+++ PS1b/src/PhotoMagic.cpp
@@ -6,18 +6,32 @@
 
 void transform(sf::Image &image, FibLFSR &L)
 {
+    int percent, progress = 0;
+    std::cout << "Processing image..." << std::endl;
+
     sf::Color p;
-    for (size_t x = 0; x < image.getSize().x; x++)
+    auto size = image.getSize();
+    for (size_t x = 0; x < size.x; x++)
     {
-        for (size_t y = 0; y < image.getSize().y; y++)
+        for (size_t y = 0; y < size.y; y++)
         {
             p = image.getPixel(x, y);
             p.r ^= L.generate(8);
             p.g ^= L.generate(8);
             p.b ^= L.generate(8);
             image.setPixel(x, y, p);
+            
+            percent = 100 * ((y + 1) + ((x + 1) * size.y))/(size.x * size.y);
+            if (percent >= progress)
+            {
+                std::cout << "\r" << "[" << std::string(percent, '=') << std::string(100 - percent, ' ') << "] ";
+                std::cout << percent << "%";
+                std::cout.flush();
+                progress++;
+            }
         }
     }
+    std::cout << "\nProcess Complete." << std::endl;
 }
 
 // display an encrypted copy of the picture, using the LFSR
@@ -37,11 +51,13 @@ int main(int argc, char *argv[])
     FibLFSR L(argv[3]);
     
     sf::Image image_in;
+    std::cout << "Loading image \"" << input_path << "\"... ";
     if (!image_in.loadFromFile(input_path))
         return -1;
     sf::Image image_out;
     image_out.create(image_in.getSize().x, image_in.getSize().y);
     image_out.copy(image_in, 0, 0);
+    std::cout << "Done." << std::endl;
 
     // Encrypts image_out using our LFSR
     transform(image_out, L);
@@ -55,7 +71,7 @@ int main(int argc, char *argv[])
     sprite_out.setTexture(texture_out);
     sprite_out.setPosition({sprite_in.getGlobalBounds().width, 0});
 
-    sf::RenderWindow window(sf::VideoMode(2 * image_in.getSize().x, image_in.getSize().y), "Input");
+    sf::RenderWindow window(sf::VideoMode(2 * image_in.getSize().x, image_in.getSize().y), "Input vs. Output Comparison");
 
     while (window.isOpen())
     {
@@ -71,6 +87,8 @@ int main(int argc, char *argv[])
         window.display();
     }
 
+    std::cout << "Saving image as \"" << output_path << "\"... ";
     if (!image_out.saveToFile(output_path))
         return -1;
+    std::cout << "Done." << std::endl;
 }
