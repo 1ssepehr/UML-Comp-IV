2a6d397d85560581f7868aefbd89a820e968ce11
Sepehr Madani <ssepehrmadani@gmail.com>
2021-02-27 19:07:47 -0500

    Add T and delta_T arguments to the NBody program

--- PS2b/src/main.cpp
+++ PS2b/src/main.cpp
@@ -1,11 +1,15 @@
 #include "Universe.hpp"
 #include "CelestialBody.hpp"
 
+#include <cstdlib>
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
-    Universe SolarSystem;
+    auto T = std::atof(argv[1]);
+    auto delta_T = std::atof(argv[2]);
+    Universe SolarSystem(T, delta_T);
+
     std::cin >> SolarSystem;
     SolarSystem.setTitle("Solar System");
     SolarSystem.load();
