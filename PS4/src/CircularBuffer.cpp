59464cabbd6f5f25605a9f24a813c7169c244947
Sepehr Madani <ssepehrmadani@gmail.com>
2021-03-20 15:24:31 -0400

    Reformat with cpplint

--- PS4/src/CircularBuffer.cpp
+++ PS4/src/CircularBuffer.cpp
@@ -1,3 +1,7 @@
+// (C) Copyright Seyedsepehr Madani, 2021.
+// Distributed under MIT license, available at
+// https://opensource.org/licenses/MIT.
+
 #include "CircularBuffer.hpp"
 
 #include <cstddef>
@@ -6,34 +10,29 @@
 #include <memory>
 #include <stdexcept>
 
-CircularBuffer::CircularBuffer(size_t capacity) : capacity(capacity), used_size(0), front(0), back(0)
-{
-    if (capacity < 1)
-        throw std::invalid_argument("CircularBuffer constructor: capacity must be greater than zero.");
-    buffer = std::make_unique<std::int16_t[]>(capacity);
+CircularBuffer::CircularBuffer(size_t capacity)
+    : capacity(capacity), used_size(0), front(0), back(0) {
+  if (capacity < 1)
+    throw std::invalid_argument(
+        "CircularBuffer constructor: capacity must be greater than zero.");
+  buffer = std::make_unique<std::int16_t[]>(capacity);
 }
 
-void CircularBuffer::enqueue(std::int16_t x)
-{
-    if (isFull())
-        throw std::runtime_error("enqueue: can't enqueue to a full ring.");
-    buffer[back] = x;
-    if (++back >= capacity)
-        back = 0;
-    ++used_size;
+void CircularBuffer::enqueue(std::int16_t x) {
+  if (isFull())
+    throw std::runtime_error("enqueue: can't enqueue to a full ring.");
+  buffer[back] = x;
+  back = (back + 1) % capacity;
+  ++used_size;
 }
 
-std::int16_t CircularBuffer::dequeue()
-{
-    if (isEmpty())
-        throw std::runtime_error("dequeue: can't dequeue an empty ring.");
-    auto value = buffer[front];
-    ++front;
-    --used_size;
-    return value;
+std::int16_t CircularBuffer::dequeue() {
+  if (isEmpty())
+    throw std::runtime_error("dequeue: can't dequeue an empty ring.");
+  auto value = buffer[front];
+  front = (front + 1) % capacity;
+  --used_size;
+  return value;
 }
 
-std::int16_t CircularBuffer::peek()
-{
-    return buffer[front];
-}
+std::int16_t CircularBuffer::peek() { return buffer[front]; }
--- PS4/src/CircularBuffer.hpp
+++ PS4/src/CircularBuffer.hpp
@@ -1,50 +1,42 @@
-#ifndef CIRCULAR_BUFFER_HPP
-#define CIRCULAR_BUFFER_HPP 1
+// (C) Copyright Seyedsepehr Madani, 2021.
+// Distributed under MIT license, available at
+// https://opensource.org/licenses/MIT.
+
+#ifndef PS4_SRC_CIRCULARBUFFER_HPP_
+#define PS4_SRC_CIRCULARBUFFER_HPP_ 1
 
 #include <cstddef>
 #include <cstdint>
 #include <memory>
 
-class CircularBuffer
-{
-  public:
-    CircularBuffer(size_t capacity);
-    CircularBuffer() : CircularBuffer(16)
-    {
-    }
+class CircularBuffer {
+ public:
+  explicit CircularBuffer(size_t capacity);
+  CircularBuffer() : CircularBuffer(16) {}
 
-    ~CircularBuffer() = default;
+  ~CircularBuffer() = default;
 
-    CircularBuffer(CircularBuffer &other) = delete;
-    CircularBuffer(CircularBuffer &&other) = delete;
+  CircularBuffer(const CircularBuffer &other) = delete;
+  CircularBuffer(CircularBuffer &&other) = delete;
 
-    CircularBuffer &operator=(const CircularBuffer &other) = delete;
-    CircularBuffer &operator=(const CircularBuffer &&other) = delete;
+  CircularBuffer &operator=(const CircularBuffer &other) = delete;
+  CircularBuffer &operator=(const CircularBuffer &&other) = delete;
 
-    size_t size() const
-    {
-        return used_size;
-    }
-    bool isEmpty() const
-    {
-        return used_size == 0;
-    }
-    bool isFull() const
-    {
-        return used_size == capacity;
-    }
-    void enqueue(std::int16_t x);
-    std::int16_t dequeue();
-    std::int16_t peek();
+  size_t size() const { return used_size; }
+  bool isEmpty() const { return used_size == 0; }
+  bool isFull() const { return used_size == capacity; }
+  void enqueue(std::int16_t x);
+  std::int16_t dequeue();
+  std::int16_t peek();
 
-  private:
-    std::unique_ptr<std::int16_t[]> buffer;
-    int front;     // Index to the first element
-    int back;      // Index of the right of the last element (first free cell, if any)
-    size_t used_size; // Number of elements in use
-    size_t capacity;  // Total capacity of the buffer
+ private:
+  std::unique_ptr<std::int16_t[]> buffer;
+  size_t front;      // Index of the first element
+  size_t back;       // Index after the last used element
+  size_t used_size;  // Number of elements in use
+  size_t capacity;   // Total capacity of the buffer
 
-    constexpr static double DECAY_FACTOR = 0.996;
+  constexpr static double DECAY_FACTOR = 0.996;
 };
 
-#endif
+#endif  // PS4_SRC_CIRCULARBUFFER_HPP_
